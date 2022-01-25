cbf802b8c99bb5837c1d298a27304449ed5b4686
light-200 <44830619+light-200@users.noreply.github.com>
2022-01-25 17:24:44 +0530

    feat/router 6 added 👨‍🚒

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './style/App.css';
-import { Feed } from './pages/feed';
-import Navbar from './components/navbar';
+import "./style/App.css";
+import { Feed } from "./pages/feed";
+import Navbar from "./components/navbar";
+import Signup from "./pages/Signup";
+import { Routes, Route, useParams } from "react-router-dom";
+import Login from "./pages/Login";
+import User from "./pages/User";
 
 function App() {
   return (
     <div className="App">
-
       <Navbar />
-      <Feed />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path=":user" element={<User />} />
+      </Routes>
     </div>
   );
 }
