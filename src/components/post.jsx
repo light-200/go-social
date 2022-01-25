1d7c863827f692e72bcf15d30e825caf363b2db1
light-200 <44830619+light-200@users.noreply.github.com>
2022-01-25 17:26:34 +0530

    fix/user profile renderer added 👨‍🚒

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,31 +1,38 @@
-import '../style/post.css';
-const Post = ({ data }) => {
-
-    return (<div className="post">
-        <div className="postHeader">
-            <div className="postBanner">
-
-            </div>
-            <div className="postProfile">
-
-            </div>
+import "../style/post.css";
+import { Link } from "react-router-dom";
+import { useState } from "react/cjs/react.production.min";
+const Post = ({ name }) => {
+  return (
+    <div className="post">
+      <div className="postHeader">
+        <div className="postBanner"></div>
+        <Link className="postProfile" to={name}></Link>
+      </div>
+      <div className="postBody">
+        <div className="postSocialMedia">
+          <a className="socialsBtn" href={"#"} target="_blank">
+            Y
+          </a>
+          <a className="socialsBtn" href={"#"} target="_blank">
+            I
+          </a>
+          <a className="socialsBtn" href={"#"} target="_blank">
+            li
+          </a>
+          <a className="socialsBtn" href={"#"} target="_blank">
+            Sp
+          </a>
         </div>
-        <div className="postBody">
-            <div className="postSocialMedia">
-                <a className='socialsBtn' href={"#"} target='_blank'>Y</a>
-                <a className='socialsBtn' href={"#"} target='_blank'>I</a>
-                <a className='socialsBtn' href={"#"} target='_blank'>li</a>
-                <a className='socialsBtn' href={"#"} target='_blank'>Sp</a>
-            </div>
-        </div>
-        <div className="postActions">
-            <span className='btn'>follow</span>
-            <span className='btn'>upvote</span>
-            <span className='btn'>save</span>
-            <span className='btn'>notInterested</span>
-            <span className='btn'>report</span>
-        </div>
-    </div>);
-}
+      </div>
+      <div className="postActions">
+        <span className="btn">follow</span>
+        <span className="btn">upvote</span>
+        <span className="btn">save</span>
+        <span className="btn">notInterested</span>
+        <span className="btn">report</span>
+      </div>
+    </div>
+  );
+};
 
 export default Post;
--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -1,31 +1,26 @@
 // @flow
-import * as React from 'react';
-import '../style/feed.css'
-import UserProfile from '../components/userProfile';
-import Post from '../components/post';
-import Notification from '../components/notification';
+import * as React from "react";
+import "../style/feed.css";
+import UserProfile from "../components/userProfile";
+import Post from "../components/post";
+import Notification from "../components/notification";
 
 export function Feed() {
-    return (
-        <div className="feedContainer">
-            <div className="feedProfile">
-                <UserProfile />
-            </div>
-            <div className="feedPosts">
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-            </div>
-            <div className="feedNotifications">
-                <div className="notificationHeader">
-                    Notifications
-                </div>
-                <Notification />
-            </div>
-        </div>
-
-    );
-};
+  var names = ["Jake", "Jon", "Thruster"];
+  return (
+    <div className="feedContainer">
+      <div className="feedProfile">
+        <UserProfile />
+      </div>
+      <div className="feedPosts">
+        {names.map((name, index) => {
+          return <Post name={name} key={index} />;
+        })}
+      </div>
+      <div className="feedNotifications">
+        <div className="notificationHeader">Notifications</div>
+        <Notification />
+      </div>
+    </div>
+  );
+}
