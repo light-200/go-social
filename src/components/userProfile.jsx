62a6c72208cafa12981ca31369a6c68c8749bab1
light-200 <44830619+light-200@users.noreply.github.com>
2022-01-25 17:27:43 +0530

    formatting done using prettier 👨‍⚕️

--- src/components/userProfile.jsx
+++ src/components/userProfile.jsx
@@ -1,39 +1,45 @@
-import React from 'react';
-import '../style/feedProfile.css';
+import React from "react";
+import "../style/feedProfile.css";
 
 export function UserProfile({ data }) {
-    return (<div className="profileCard">
-        <div className="profileHeader">
-            <div className="profileBanner">
-
-            </div>
-            <div className="profilePicture">
-
-            </div>
+  return (
+    <div className="profileCard">
+      <div className="profileHeader">
+        <div className="profileBanner"></div>
+        <div className="profilePicture"></div>
+      </div>
+      <div className="profileBody">
+        <div className="profileStats">
+          <div className="followers">
+            <div>followers</div>
+            <span>10</span>
+          </div>
+          <div className="following">
+            <div>following</div>
+            <span>20</span>
+          </div>
         </div>
-        <div className="profileBody">
-            <div className="profileStats">
-                <div className="followers">
-                    <div>followers</div>
-                    <span>10</span>
-                </div>
-                <div className="following">
-                    <div>following</div>
-                    <span>20</span>
-                </div>
-            </div>
-            <div className="profileIcons">
-                <a className='socialsBtn' target='_blank'>Y</a>
-                <a className='socialsBtn' target='_blank'>I</a>
-                <a className='socialsBtn' target='_blank'>li</a>
-                <a className='socialsBtn' target='_blank'>Sp</a>
-                <a className='socialsBtn' target='_blank'>+</a>
-            </div>
+        <div className="profileIcons">
+          <a className="socialsBtn" target="_blank">
+            Y
+          </a>
+          <a className="socialsBtn" target="_blank">
+            I
+          </a>
+          <a className="socialsBtn" target="_blank">
+            li
+          </a>
+          <a className="socialsBtn" target="_blank">
+            Sp
+          </a>
+          <a className="socialsBtn" target="_blank">
+            +
+          </a>
         </div>
-        <div className="profileFooter">
-            this is the profile description
-        </div>
-    </div>);
+      </div>
+      <div className="profileFooter">this is the profile description</div>
+    </div>
+  );
 }
 
 export default UserProfile;
